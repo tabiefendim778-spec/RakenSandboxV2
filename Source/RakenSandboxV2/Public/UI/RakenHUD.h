40f2c72115e101b7e0c6a05578e4a46faac832f2
tabiefendim778-spec <tabiefendim778@gmail.com>
2026-09-25 13:46:24 +0300

    Add toggleable celestial size comparison mode

--- Source/RakenSandboxV2/Public/UI/RakenHUD.h
+++ Source/RakenSandboxV2/Public/UI/RakenHUD.h
@@ -12,6 +12,12 @@ class RAKENSANDBOXV2_API ARakenHUD : public AHUD
 public:
     virtual void DrawHUD() override;
 
+    UFUNCTION(BlueprintCallable, Category="RAKEN|UI")
+    void ToggleSizeComparison() { bShowSizeComparison = !bShowSizeComparison; }
+
 private:
+    bool bShowSizeComparison = false;
+
     void DrawTextLine(const FString& Text, float& Y, const FLinearColor& Color, float Scale = 1.0f);
+    void DrawSizeComparison();
 };
