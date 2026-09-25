fc1cc901a3a88008fe35ce9ba004f71e880f1c32
tabiefendim778-spec <tabiefendim778@gmail.com>
2026-09-25 12:53:53 +0300

    Add Source/RakenSandboxV2/Public/Celestial/RakenCelestialBody.h

---
+++ Source/RakenSandboxV2/Public/Celestial/RakenCelestialBody.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Celestial/RakenCelestialTypes.h"
+#include "RakenCelestialBody.generated.h"
+
+class UStaticMeshComponent;
+
+UCLASS()
+class RAKENSANDBOXV2_API ARakenCelestialBody : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ARakenCelestialBody();
+
+    virtual void Tick(float DeltaSeconds) override;
+
+    void BindToState(const FRakenCelestialState& State);
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="RAKEN")
+    TObjectPtr<UStaticMeshComponent> BodyMesh;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="RAKEN")
+    FGuid BodyId;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RAKEN|Rendering")
+    double MinimumVisualRadiusCm = 16.0;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RAKEN|Rendering")
+    double MaximumVisualRadiusCm = 1800.0;
+
+private:
+    void ApplyState(const FRakenCelestialState& State);
+    double ComputeVisualRadiusCm(const FRakenCelestialState& State) const;
+};
