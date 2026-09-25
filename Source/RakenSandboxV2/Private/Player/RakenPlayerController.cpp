af76a2d1e3447893f3b05d2d5655b4d3fd5b7eca
tabiefendim778-spec <tabiefendim778@gmail.com>
2026-09-25 13:34:49 +0300

    Implement pause and time-scale controls

--- Source/RakenSandboxV2/Private/Player/RakenPlayerController.cpp
+++ Source/RakenSandboxV2/Private/Player/RakenPlayerController.cpp
@@ -1,6 +1,7 @@
 #include "Player/RakenPlayerController.h"
 
 #include "Celestial/RakenCelestialBody.h"
+#include "Simulation/RakenSimulationSubsystem.h"
 
 ARakenPlayerController::ARakenPlayerController()
 {
@@ -20,6 +21,10 @@ void ARakenPlayerController::SetupInputComponent()
             IE_Pressed,
             this,
             &ARakenPlayerController::SelectUnderCursor);
+
+        InputComponent->BindAction(TEXT("PauseSimulation"), IE_Pressed, this, &ARakenPlayerController::TogglePause);
+        InputComponent->BindAction(TEXT("FasterTime"), IE_Pressed, this, &ARakenPlayerController::FasterTime);
+        InputComponent->BindAction(TEXT("SlowerTime"), IE_Pressed, this, &ARakenPlayerController::SlowerTime);
     }
 }
 
@@ -39,4 +44,28 @@ void ARakenPlayerController::SelectUnderCursor()
     }
 
     SelectedBody = Cast<ARakenCelestialBody>(Hit.GetActor());
+}
+
+void ARakenPlayerController::TogglePause()
+{
+    if (URakenSimulationSubsystem* Simulation = GetWorld()->GetSubsystem<URakenSimulationSubsystem>())
+    {
+        Simulation->bPaused = !Simulation->bPaused;
+    }
+}
+
+void ARakenPlayerController::FasterTime()
+{
+    if (URakenSimulationSubsystem* Simulation = GetWorld()->GetSubsystem<URakenSimulationSubsystem>())
+    {
+        Simulation->TimeScale = FMath::Clamp(Simulation->TimeScale * 2.0, 0.25, 1.0e8);
+    }
+}
+
+void ARakenPlayerController::SlowerTime()
+{
+    if (URakenSimulationSubsystem* Simulation = GetWorld()->GetSubsystem<URakenSimulationSubsystem>())
+    {
+        Simulation->TimeScale = FMath::Clamp(Simulation->TimeScale * 0.5, 0.25, 1.0e8);
+    }
 }
