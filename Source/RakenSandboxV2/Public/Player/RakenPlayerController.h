cedad164f395fefe84d51e9ee0a16baceab0a79c
tabiefendim778-spec <tabiefendim778@gmail.com>
2026-09-25 13:32:00 +0300

    Add celestial selection controller

---
+++ Source/RakenSandboxV2/Public/Player/RakenPlayerController.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RakenPlayerController.generated.h"
+
+class ARakenCelestialBody;
+
+UCLASS()
+class RAKENSANDBOXV2_API ARakenPlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+public:
+    ARakenPlayerController();
+
+    UFUNCTION(BlueprintPure, Category="RAKEN|Selection")
+    ARakenCelestialBody* GetSelectedBody() const { return SelectedBody.Get(); }
+
+    UFUNCTION(BlueprintCallable, Category="RAKEN|Selection")
+    void ClearSelection();
+
+protected:
+    virtual void SetupInputComponent() override;
+
+private:
+    UPROPERTY()
+    TObjectPtr<ARakenCelestialBody> SelectedBody;
+
+    void SelectUnderCursor();
+};
